ae2df9890de9d0b6f101144cb22d19dcc34a98f9
Aaron Curtis <foobarbecue@gmail.com>
2019-03-24 16:47:26 -0700

    Make it work with column names other than x and y

--- src/Plot.js
+++ src/Plot.js
@@ -2,17 +2,27 @@ import {XYPlot, XAxis, YAxis, LineSeries} from 'react-vis'
 import React, { Component } from 'react'
 
 export default class extends Component{
+	constructor(props){
+		super()
+		// React-vis wants objects with keys "x" and "y", so rename.
+		// Field names are still in props.plotData.meta.fields
+		this.data = props.plotData.data.map(
+			(row)=>({
+				x: row[props.plotData.meta.fields[0]],
+				y: row[props.plotData.meta.fields[1]]
+			}))
+	}
+
 	render() {
-		console.log(this.props.plotData.data)
 			return (
 				<>
 				<XYPlot
 					width={300}
 					height={300}>
 					<LineSeries
-						data={this.props.plotData.data}/>
-					<XAxis/>
-					<YAxis/>
+						data={this.data}/>
+					<XAxis title={this.props.plotData.meta.fields[0]}/>
+					<YAxis title={this.props.plotData.meta.fields[1]}/>
 				</XYPlot>
 				</>
 			)
