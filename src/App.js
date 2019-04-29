b30d1f6ea9da5fd908782d6d83469461815f8859
Aaron Curtis <foobarbecue@gmail.com>
2019-04-28 21:52:40 -0700

    Work towards getting css grid layout going for data panes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import {dataPanes} from './stores'
 import SplTimeline from './SplTimeline'
 import SplDataPane from './SplDataPane'
 import SplAddData from './SplAddData'
+import './App.css'
 
 const App = view(() => {
 	return (
 		<div>
-			<SplAddData	/>
+
+			<div style = {{
+				position:'fixed',
+				top:'0',
+				height:'150px',
+				width:'100%'
+			}}>header
+				<SplAddData 	/>
+			</div>
 			<div style={{
 				display: 'grid',
 				gridGap: '20px',
-				gridTemplateColumns: 'repeat(auto-fit, 400px)',
-				height: '400px',
+				// gridTemplateRows: 'repeat(auto-fill, 400px)',
+				gridTemplateColumns: 'repeat(auto-fill, 1fr)',
+				gridAutoFlow: 'column',
+				position: 'absolute',
+				top: '150px',
+				bottom: '150px',
+				width: '100%'
 				// overflow: 'hidden'
 			}}>
 				{dataPanes.all.map(pane =>
@@ -23,7 +37,15 @@ const App = view(() => {
 					/>
 				)}
 			</div>
-			<SplTimeline/>
+			<div id={"timeline"} style = {{
+				position:'fixed',
+				bottom:'0',
+				height:'150px',
+				width:'100%'
+			}}>
+			<SplTimeline
+			/>
+			</div>
 		</div>
 	)
 });
