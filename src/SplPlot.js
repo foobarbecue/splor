4ac2061fc75cc7dda1d834fd0e3a3cb479763dd6
Aaron Curtis <foobarbecue@gmail.com>
2019-05-26 21:20:50 -0700

    Make seeking on timeline seek on video
    Involved turning SplPlayer into a class-based view instead of functional
    so that I could use a ref

--- src/SplPlot.js
+++ src/SplPlot.js
@@ -67,7 +67,7 @@ class OneLineTSPlot extends Component {
 						data={this.data}/>
 					<Crosshair
 						values={[{x: timeCursor.getTime(), y: 0}]}
-					>test</Crosshair>
+					>{timeCursor.getTime()}</Crosshair>
 					<Borders style={{all: {fill: '#fff'}}} />
 					<XAxis title={this.props.meta.fields[0]}/>
 					<YAxis title={this.props.meta.fields[1]}/>
